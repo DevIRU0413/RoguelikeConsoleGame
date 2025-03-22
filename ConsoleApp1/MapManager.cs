f99d11f46a8443732cadbbdc3792b6cbfe04641c
[200~ <deviru0413@gmail.com>
2025-03-22 14:50:25 +0900

    MapManager 구현
    [0.0.4]
    맵 매니저 구현.
    - 맴 정보
    - 맵 그리기

---
+++ ConsoleApp1/MapManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Enums;
+
+public class MapManager
+{
+    // 싱클톤
+    private static MapManager managerSingleton;
+    public static MapManager Instance()
+    {
+        if(managerSingleton == null)
+        {
+            managerSingleton = new MapManager();
+        }
+        return managerSingleton;
+    }
+
+    // Tiles Data
+    Tile tile = null;
+
+    public void SetTiles()
+    {
+
+    }
+}
